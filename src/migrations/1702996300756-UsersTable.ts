b530806302a89684bf39ca3ecd67998287427698
wind-of <adequation@mail.ru>
2023-12-20 05:07:23 +0300

    feat: миграция для создания таблицы пользователей

---
+++ src/migrations/1702996300756-UsersTable.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UsersTable1702996300756 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        firstname VARCHAR(255) NOT NULL,
+        lastname VARCHAR(255),
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        avatar VARCHAR(255)
+      )
+    `);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`DROP TABLE IF EXISTS users`);
+	}
+}
