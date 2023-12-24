a369c435f569560d8896f6bd229123e83e7ed85b
wind-of <adequation@mail.ru>
2023-12-24 04:40:09 +0300

    feat(guard): `guard` для проверки существования проекта с переданным `id`

---
+++ src/project/guards/project-existence.guard.ts
@@ -0,0 +1,19 @@
+import {
+	CanActivate,
+	ExecutionContext,
+	Inject,
+	Injectable
+} from "@nestjs/common";
+import { ProjectService } from "src/project/project.service";
+
+@Injectable()
+export class ProjectExistenceGuard implements CanActivate {
+	constructor(@Inject(ProjectService) private projectService: ProjectService) {}
+	async canActivate(context: ExecutionContext) {
+		const request = context.switchToHttp().getRequest();
+		const project = await this.projectService.findOneById(
+			request.params.projectId
+		);
+		return !!project;
+	}
+}
